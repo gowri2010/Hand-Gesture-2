9fca78d3ea82b6c60317bae19edb73a20e61f08c
gowri2010 <86656713+gowri2010@users.noreply.github.com>
2022-01-31 20:50:05 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+     document.getElementById("result").innerHTML = '<img id="captured_image" src+"'+data_uri+'"/>';
+    })
+}
+
+console.log('ml5 version: https://unpkg.com/ml5@0.4.3/dist/ml5.min.j', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JUEnG4Qih/model.json', modelloaded);
+
+function modelloaded() {
+    console.log('Model Loaded!');
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction;
+    var utterThis = new SpeechSynthesisUtterance_(speak_data);
+    synth.speak(utterThis);
+}
