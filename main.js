b5990ef576942f514e463a0b4762597dfff55280
gowri2010 <86656713+gowri2010@users.noreply.github.com>
2022-02-01 15:50:01 +1000

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function take_snapshot()
     })
 }
 
-console.log('ml5 version: ', ml5.version);
+console.log('ml5 version:', ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JUEnG4Qih/model.json', modelloaded);
 
